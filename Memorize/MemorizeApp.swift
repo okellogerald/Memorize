8ec4f7d4bba4f11e633b167ab4a5e572eccf327c
Okello Gerald <okellogeralddev@gmail.com>
2023-03-11 23:09:34 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by MacBookPro on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
